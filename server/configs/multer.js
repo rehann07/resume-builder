88c0fe395af112dd58b64c0eaefbfcb7c3cf6d2d
Rehan Naikwadi <rehannaikwadi07@gmail.com>
2026-04-06 15:21:31 +0530

    Add .gitignore and initial project files

---
+++ server/configs/multer.js
@@ -0,0 +1,25 @@
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+  if (file.mimetype === "application/pdf") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only PDF files are allowed"), false);
+  }
+},
+  limits: { fileSize: 2 * 1024 * 1024 }
+});
+
+export default upload;
